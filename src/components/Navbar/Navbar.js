528a2b65595df642d71c22c2ebb641f1a4208539
jonathancode <parkj97@gmail.com>
2021-09-21 13:45:33 -0700

    navbar basic

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,19 @@
-import './Navbar.css';
+import React, {useState} from 'react';
+
+import "./Navbar.css";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
+    const [displayItems, setDisplayItems] = useState(false);
+
+    const displayHandler = () => {
+        setDisplayItems(!displayItems)
+    }
+
   return (
     <div className="navbar">
-      <div>Logo</div>
-      <div>
+      <div className="navbar__logo">Logo</div>
+      <div className="navbar__experiences">
         {/* search : Location / Check-in / Check-out / Guests*/}
         <a href="#">Places to Stay</a>
         {/*  Enable users to put up their own listing*/}
@@ -12,15 +21,17 @@ const Navbar = () => {
         {/* Reviews from recent users */}
         <a href="#">Browse Experiences</a>
       </div>
-      <div>
-        <a href="#">Sign Up</a>
-        <a href="#">Log In</a>
-        <hr />
-        <a href="#">Host your home</a>
+      <div className="navbar__dropdown">
+        <MenuIcon onClick={displayHandler}/>
+        {displayItems ? <div className="navbar__dropdown__content">
+          <a href="#">Sign Up</a>
+          <a href="#">Log In</a>
+          <hr />
+          <a href="#">Host your home</a>
+        </div> : <div className="navbar__dropdown__content"></div>}
       </div>
     </div>
   );
 };
 
 export default Navbar;
-
